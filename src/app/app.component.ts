4c4a7f1ffe3e9a2e9f4d8e46cd19b199a28fd44d
AkshatRamanathan <ramanathan.ak@northeastern.edu>
2024-04-06 15:32:55 -0400

    routing end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink,NavComponent,FooterComponent,LowerCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Test Application';
+  title = 'SPA APP';
 }
